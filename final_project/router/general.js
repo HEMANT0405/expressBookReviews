b88fb72b9216106e84d02b6254c79569c260adbc
HEMANT0405 <hemantshinde2003.2006@gmail.com>
2026-08-31 22:04:30 +0530

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -34,9 +34,10 @@ public_users.get('/author/:author', function (req, res) {
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/title/:title', function (req, res) {
+  const title = req.params.title;
+  const result = Object.values(books).filter(book => book.title === title);
+  res.json(result);
 });
 
 //  Get book review
