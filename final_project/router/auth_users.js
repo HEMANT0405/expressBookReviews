e5456763571bfb15ac24df9f7269b6ec4f9a25af
HEMANT0405 <hemantshinde2003.2006@gmail.com>
2026-08-31 22:15:34 +0530

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -9,14 +9,30 @@ const isValid = (username) => {
   return users.some(user => user.username === username);
 };
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
-}
+const authenticatedUser = (username, password) => {
+  return users.some(user => user.username === username && user.password === password);
+};
 
 //only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+regd_users.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  if (authenticatedUser(username, password)) {
+    const token = jwt.sign(
+      { username: username },
+      "fingerprint_customer",
+      { expiresIn: "1h" }
+    );
+
+    return res.status(200).json({
+      message: "Login successful",
+      token: token
+    });
+  }
+
+  return res.status(401).json({
+    message: "Invalid username or password"
+  });
 });
 
 // Add a book review
